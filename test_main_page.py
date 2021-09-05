6e9091085e96d73a28536befa837cdb7f4793e5c
AlexandraNayanzina <alexandranayanzina@gmail.com>
2021-09-05 17:25:00 +0300

    files base_page.py and main_page.py were added,Page Object is initialized in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,19 @@
 import time
+from pages.main_page import MainPage
 
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    # browser.get(link)
+    # go_to_login_page(browser)
+
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
     time.sleep(5)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
